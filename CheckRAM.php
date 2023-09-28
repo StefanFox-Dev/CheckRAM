f2a6c9df57d2e897b5793ffad2323d7b2ee0d48a
StefanFox <59245729+StefanFox-Dev@users.noreply.github.com>
2023-09-28 20:54:34 +0300

    Update CheckRAM

--- CheckRAM.php
+++ CheckRAM.php
@@ -1,42 +1,36 @@
 <?php
-$time = 2; //second timer
-$availableRam_id = 0;
-$totalRam = 0;
-$availableRam = 0;
+$availableRam_id = 0; $totalRam = 0; $availableRam = 0; $txt = "Скрипт проверки использования оперативной памяти запущен!
+Работает только на ОС: Linux и Android";
 
-function send(string $msg = ''): void {
-    $idChatTG = 0;
-    $idChatVK = 0;
-    $keyBotTG = 'bot58:an';
+#Отправка сообщений в Телеграм, ВК / Send message to Telegram, VK
+function send(string $message = ''): void {
+    $token = 'bot1234:TEST-test'; #токен бота / token bot
+    $id = '-10071'; #айди чата / chat id
 
-    //TG
-    $url = 'https://api.telegram.org/' . (string)$keyBotTG . '/sendMessage?chat_id=' . (int)$idChatTG . '&text=' . urlencode($msg);
+    $url = 'https://api.telegram.org/' . $token . '/sendMessage?chat_id=' . $id . '&text=' . urlencode($message);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     curl_close($ch);
 
-    //VK
     $url = 'https://api.vk.com/method/messages.send';
     $params = [
-        'access_token' => 'vk1.a.',
-        //key group
+        'access_token' => 'vk1.a.', #токен / token
         'v' => '5.85'
     ];
-    $params['message'] = $msg;
-    $params['chat_id'] = (int)$idChatVK;
+    $params['message'] = $message;
+    $params['chat_id'] = 1; #айди чата / id chat
     $ch = curl_init($url . '?' . http_build_query($params));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
 }
 
-echo 'Скрипт проверки использования оперативной памяти запущен!
-Работает только на ОС: Linux и Android' . PHP_EOL;
+echo $txt;
+
+#ЗАПУСК / START
 while (true) {
-    sleep($time);
-    
     $info = @file_get_contents('/proc/meminfo');
 
      if (preg_match('/^MemTotal:\s+(\d+)\skB$/m', $info, $matches)) {
@@ -50,7 +44,7 @@ while (true) {
     if((int)$availableRam < 1){
         $message = date('d-m-Y H:i:s') . ' | 🔴 Не осталось свободного места в ОЗУ: ' . $availableRam . 'ГБ, было: ' . $availableRam_id . 'ГБ из ' . $totalRam . ' ГБ' . PHP_EOL;
         
-         echo $message;
+         echo PHP_EOL . $message;
          send($message);
     }
     
@@ -58,7 +52,9 @@ while (true) {
         $message = date('d-m-Y H:i:s') . ' | Осталось свободного места в ОЗУ: ' . $availableRam . 'ГБ, было: ' . $availableRam_id . 'ГБ из ' . $totalRam . ' ГБ' . PHP_EOL;
         
          $availableRam_id = $availableRam;
-         echo $message;
+         echo PHP_EOL . $message;
     }
 }
+
+sleep(2); #время проверки / time check
 ?>
